625a8dc8748ee910a5a088d721b3a612340361d9
Evan Feenstra <evan@evanfeenstra.com>
2018-01-07 19:35:30 -0800

    real data two ways

--- ble_peripheral/ble_peripheral.ino
+++ ble_peripheral/ble_peripheral.ino
@@ -17,8 +17,8 @@
 
 BLEPeripheral blePeripheral = BLEPeripheral(BLE_REQ, BLE_RDY, BLE_RST);
 BLEService ledService = BLEService("00001234-0000-1000-8000-00805f9b34fb");
-BLECharacteristic characteristic = BLECharacteristic("00001234-0000-1000-8000-00805f9b34fb", BLEWriteWithoutResponse | BLENotify, BLE_ATTRIBUTE_MAX_VALUE_LENGTH);
-BLEDescriptor descriptor = BLEDescriptor("00001234-0000-1000-8000-00805f9b34fb", "FogNet");
+BLECharacteristic characteristic = BLECharacteristic("00001234-0000-1000-8000-00805f9b34fb", BLEWriteWithoutResponse, BLE_ATTRIBUTE_MAX_VALUE_LENGTH);
+BLECharacteristic notifyChar = BLECharacteristic("00001235-0000-1000-8000-00805f9b34fb", BLENotify, BLE_ATTRIBUTE_MAX_VALUE_LENGTH);
 
 void setup() {
   Wire.begin();
@@ -33,14 +33,13 @@ void setup() {
   // set advertised local name and service UUID
   blePeripheral.setLocalName("FogNet");
   blePeripheral.setAdvertisedServiceUuid(ledService.uuid());
-
-  // add service and characteristic
   blePeripheral.addAttribute(ledService);
   blePeripheral.addAttribute(characteristic);
+  blePeripheral.addAttribute(notifyChar);
 
   // assign event handlers for connected, disconnected to peripheral
-  blePeripheral.setEventHandler(BLEConnected, blePeripheralConnectHandler);
-  blePeripheral.setEventHandler(BLEDisconnected, blePeripheralDisconnectHandler);
+  //blePeripheral.setEventHandler(BLEConnected, blePeripheralConnectHandler);
+  //blePeripheral.setEventHandler(BLEDisconnected, blePeripheralDisconnectHandler);
   
   // assign event handlers for characteristic
   characteristic.setEventHandler(BLEWritten, characteristicWritten);
@@ -51,30 +50,23 @@ void setup() {
   Serial.println(F("BLE LED Peripheral"));
 }
 
-elapsedMillis timeElapsed = 10001;
+elapsedMillis timeElapsed = 101;
 int idx = 0;
 
 void loop() {
-  // poll peripheral
   blePeripheral.poll();
-  if (timeElapsed > 10000) {
-    Wire.beginTransmission(8);
-    Wire.write("x is ");
-    Wire.write(idx);
-    Wire.endTransmission();
-    characteristic.setValue("HITEST");
-    idx++;
+  delay(1);
+  if(timeElapsed>5000){
+    //pollWire();
+    pollWire2();
     timeElapsed = 0;
   }
-  pollWire();
 }
 
 void blePeripheralConnectHandler(BLECentral& central) {
-  // central connected event handler
-  Serial.print(F("Connected event, central: "));
-  Serial.println(central.address());
   Wire.beginTransmission(8);
-  Wire.write("<*>connected<^>");
+  Wire.write("<*>connected        ");
+  Wire.write("<^>");
   Wire.endTransmission();
 }
 
@@ -90,21 +82,63 @@ void characteristicWritten(BLECentral& central, BLECharacteristic& characteristi
   char msg[20];
   sprintf(msg, "%20c", NULL);
   memcpy(msg, (char*)characteristic.value(), characteristic.valueLength());
-  //const unsigned char* val = characteristic.value();
   Wire.beginTransmission(8); // transmit to device #8
   Wire.write(msg);
   Wire.endTransmission();
-  //led = characteristic.value();
 }
 
+char* receiveBuffer;
 void pollWire() {
-  //Wire.requestFrom(8,20);
-  char* x;
-  while (0 < Wire.available()) { 
+  char* buff;
+  //int n = 0;
+  int n = Wire.requestFrom(8,20); // THIS IS SPAMMING
+  if(n==20) {
+    Wire.readBytes(buff, n);
+    if(buff!=receiveBuffer){
+      notifyChar.setValue(buff);
+      receiveBuffer = buff;
+    }
+  }
+  /*while(Wire.read()) {
+    num = Wire.receive();
+  }*/
+  //char* x = Wire.read();
+  //notifyChar.setValue(x);
+  /*char* x;
+  while (Wire.available() > 0) { 
     char c = Wire.read();
     x += c;
-  }
-  if(strlen(x)>0){
-    characteristic.setValue(x);
-  } 
+    if(x[0]!=0 && x[1]!=0 && x[2]!=0){
+      notifyChar.setValue(x);
+    }
+  }*/
 }
+
+void pollWire2() {
+  int n = Wire.requestFrom(8,20);
+  String buff = "";
+  //int n=0;
+  while( Wire.available() > 0 ) {
+    char c = Wire.read();
+    buff += c;
+  }
+  if(buff!=""){
+    //buff[19]='\0';
+    //char a[20];
+    //sprintf(a,buff);
+    //char msg[20];
+    //sprintf(buff, "%20c", NULL);
+    char msg[20];
+    buff.toCharArray(msg, 20);
+    notifyChar.setValue((unsigned char*)msg,20);
+    //notifyChar.setValue(buff);
+  } 
+  /*if(n==20) {
+    Wire.readBytes(buff, n);
+    if(strcmp(buff, receiveBuffer) != 0){
+      notifyChar.setValue(buff);
+      receiveBuffer = buff;
+    }
+  }*/
+}
+
--- teensyReceive/teensyReceive.ino
+++ teensyReceive/teensyReceive.ino
@@ -12,25 +12,33 @@ void setup() {
   pinMode(led, OUTPUT);
   pinMode(led2, OUTPUT);
   pinMode(led3, OUTPUT);
-  digitalWrite(led, HIGH);
+  digitalWrite(led3, HIGH);
   delay(100);
-  digitalWrite(led, LOW);
+  digitalWrite(led3, LOW);
   Wire.begin(8);                
   Wire.onReceive(receiveBle); 
+  Wire.onRequest(bleRequest); 
   Serial.begin(9600);
   Serial.println("teensy started"); 
   lcd.begin(16, 2);
   lcd.setCursor(0, 0);
-  lcd.print("hi");
+  lcd.print("ho");
   delay(1000);
   lcd.clear();
 }
 
-elapsedMillis timer;
+elapsedMillis timer = 5001;
+int ins = 0;
 void loop() {
   delay(1);
-  if (Serial.available() > 0) {
-   //digitalWrite(led, HIGH);
+  lcd.setCursor(0, 1);
+  lcd.print(ins);
+  if(timer>5000){
+    Serial.println("TEENSY HEARTBEAT");
+    timer = 0;
+  }
+  /*if (Serial.available() > 0) {
+   digitalWrite(led3, HIGH);
    String val = "";
    bool returned = false;
    while(1) {
@@ -39,8 +47,6 @@ void loop() {
      if (incomingByte == '\n' && returned==true) {
       digitalWrite(led, HIGH);
       receiveSerial(val);
-      lcd.setCursor(0, 0);
-      lcd.print(val);
       break;
      }
      if (incomingByte == '\r') {
@@ -52,23 +58,85 @@ void loop() {
      if (incomingByte == -1) continue;
      val += incomingByte;
    }
-   //digitalWrite(led, LOW);
-  }
-  lcd.setCursor(0, 1);
-  lcd.print(timer/1000);
+   digitalWrite(led3, LOW);
+  }*/
 }
 
-void receiveSerial(String val) {
+char* serialBuffer;
+int outs = 0;
+/*void receiveSerial(String val) {
   char c[20];
   val.toCharArray(c, 20);
+  serialBuffer = c;
+  outs++;
+  lcd.setCursor(0, 0);
+  lcd.print(val);
+  lcd.setCursor(8, 1);
+  lcd.print(outs);
+  //Wire.write(serialBuffer);
+}*/
+
+void bleRequest(){
+  String hi = "hihihihi";
+  char c[20];
+  hi.toCharArray(c, 20);
   Wire.write(c);
+  /*if (Serial.available() > 0) {
+   String val = "";
+   bool returned = false;
+   while(1) {
+     char incomingByte = Serial.read();
+     // '\r\n' to break
+     if (incomingByte == '\n' && returned==true) {
+      char c[20];
+      val.toCharArray(c, 20);
+      //Wire.write(c);
+      Wire.write("hi testing");
+      break;
+     }
+     if (incomingByte == '\r') {
+      returned = true;
+     } else {
+      returned = false;
+     }
+     if (incomingByte == -1) continue;
+     val += incomingByte;
+   }
+  }*/
 }
 
-void receiveBle(int howMany) {
-  while (1 < Wire.available()) { 
-    char c = Wire.read(); 
-    Serial.print(c);
+String wireBuff;
+void receiveBle(int howMany) { // also runs on requestfrom???
+  String buff;
+  //Serial.println(howMany);
+  //ins = 10001;
+  /*for(int i=0; i<howMany; i++){
+    char c = Wire.read();
+    buff += c; 
   }
-  char x = Wire.read();
-  Serial.println(x);
+  while(Wire.read()) {
+    buff += Wire.receive();
+  }
+  ins++;
+  lcd.setCursor(0, 1);
+  lcd.print(ins);*/
+  
+  /*if(buff[0]!=0){
+    Serial.print(buff);
+  }*/
+  
+  while (1 < Wire.available()) {
+    char c = Wire.read();
+    buff += c;
+    //Serial.print(c);
+  }
+  if(Wire.available()==1){
+    char x = Wire.read();
+    buff += x;
+    //if(buff!=){      
+      ins++;
+      Serial.println(buff);
+      Wire.flush();
+    //}
+  }
 }
